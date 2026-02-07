648ccf4ef5b36a425afde7ca3a310649dfabe14c
Joey Ladd <joeladd96@gmail.com>
2026-02-07 00:03:10 -0700

    temp disable pwa to flush cache

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const withPWAInit: any = require("next-pwa");
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  disable: true,
 });
 
 const nextConfig: NextConfig = {
